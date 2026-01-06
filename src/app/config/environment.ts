7d33bc4d664d477146c4bb1e1be31c1a8903f28d
TranBaBinh <binhtbhe173244@fpt.edu.vn>
2026-01-06 15:08:55 +0700

    fix api

--- src/app/config/environment.ts
+++ src/app/config/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'https://bachhoammo.com.vn/api',
+  apiUrl: 'https://api.bachhoammo.com.vn/api',
   production: true,
   googleClientId: '455053755634-5q6m7osgnus5n6oa4fot14fs75r72d3j.apps.googleusercontent.com'
 };
